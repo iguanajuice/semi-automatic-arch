7f9700f0a009bd6afccfb9ceb64d604762773244
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-16 18:44:48 -0400

    Update semi-automatic-arch.sh

--- semi-automatic-arch.sh
+++ semi-automatic-arch.sh
@@ -56,8 +56,8 @@ echo "#!/bin/sh
 	pacman-key --lsign-key 3056513887B78AEB
 	pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 	echo '
-	[chaotic-aur]
-	Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+	#[chaotic-aur]
+	#Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
 	
 	sed -ei 's/#Parallel/Parallel/g' /etc/pacman.conf
 	sed -ei 's/#Color/Color/g' /etc/pacman.conf
