3776cb7c7a84a52c1f32e367f661d638bce04610
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-17 02:40:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,12 @@ LIBVA=mesa        # Driver for hardware video encoding/decoding: Radeon=mesa, In
 
 sed -ie 's/#Parallel/Parallel/g' /etc/pacman.conf # haha package download go brrrrr
 pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers $UCODE doas $_EDITOR $_SHELL `# Core packages` \
-	grub efibootmgr                                                      `# Bootloader packages` \
+	grub efibootmgr                                                                        `# Bootloader packages` \
 	git wget htop neofetch man-db usbutils arch-install-scripts                            `# Miscellaneous CLI tools` \
 	btrfs-progs lvm2 ntfs-3g gvfs-mtp                                                      `# Support additional filesystem types` \
-	networkmanager net-tools wireless_tools                              `# Networking packages` \
-	wireplumber pipewire-pulse pipewire-jack                             `# Audio packages` \
-	libva-$LIBVA-driver gstreamer-vaapi                                  `# Hardware video codecs`
+	networkmanager net-tools wireless_tools                                                `# Networking packages` \
+	wireplumber pipewire-pulse pipewire-jack                                               `# Audio packages` \
+	libva-$LIBVA-driver gstreamer-vaapi                                                    `# Hardware video codecs`
 
 genfstab -U /mnt > /mnt/etc/fstab
 if [ $_SHELL = fish ]
@@ -35,11 +35,10 @@ echo "#!/bin/sh
 Password for root:'
 	while true; do passwd && break; done
 	chsh -s /bin/"$_SHELL"
-	useradd -m "$_USER"
+	useradd -m "$_USER" -s /bin/"$_SHELL" -G wheel
 	echo '
 Password for "$_USER"'
 	while true; do passwd "$_USER" && break; done
-	usermod -s /bin/"$_SHELL" -aG wheel "$_USER"
 
 	echo '
 Uncomment your keyboard locale from the upcoming list...press enter to continue'
