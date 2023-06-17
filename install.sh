08e92c33074423e609f37b1602df8ff23256cf00
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-16 23:53:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers $UCODE doas $_
 	lvm2 ntfs-3g                                                         `# Support additional filesystem types` \
 	networkmanager net-tools wireless_tools                              `# Networking packages` \
 	wireplumber pipewire-pulse pipewire-jack                             `# Audio packages` \
-	#libva-$LIBVA-driver gstreamer-vaapi                                  `# Hardware video codecs`
+	libva-$LIBVA-driver gstreamer-vaapi                                  `# Hardware video codecs`
 
 genfstab -U /mnt > /mnt/etc/fstab
 if [ $_SHELL = fish ]
@@ -60,7 +60,7 @@ Edit the GRUB configuration if you like...press enter to continue'
 	
 	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 	pacman-key --lsign-key 3056513887B78AEB
-	pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 	echo '
 #[chaotic-aur]
 #Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
@@ -79,7 +79,7 @@ About to edit pacman config, optional repos can be enabled at the bottom by unco
 	echo kernel.sysrq=1 > /etc/sysctl.d/kernel.conf
 	systemctl enable NetworkManager
 
-	echo All done, press enter and run command \`reboot\` to restart your system'
+	echo All done, press enter and run command \`reboot\` to restart your system
 	read
  	rm setup.sh
 	exit
