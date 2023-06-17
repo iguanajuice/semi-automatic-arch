34f00a61259a012613d0c63835c965052072193a
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-17 02:24:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#HAVEREAD=1 # Uncomment if you have read this script
+HAVEREAD=0 # Set this to 1 if you have read this script
 
-if [ $HAVEREAD != 1 ]
+if [ $HAVEREAD = 0 ]
 	then echo Please read through semi-automatic-arch/install.sh and configure it where necessary
 	exit
 fi
