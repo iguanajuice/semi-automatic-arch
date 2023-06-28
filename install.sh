2ce2f8bd4953308a29023b0d285c3d12be3c68ab
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-28 07:59:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,8 @@ pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers $UCODE doas $_
 	networkmanager net-tools wireless_tools                                `# Networking packages` \
 	wireplumber pipewire-pulse pipewire-jack                               `# Audio packages` \
 	libva-$LIBVA-driver gstreamer-vaapi                                    `# Hardware video codecs` \
- 	ttf-droid ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji        `# Wider unicode character support`
+ 	libertinus-font ttf-liberation ttf-ubuntu-font-family ttf-dejavu \
+  	ttf-droid noto-fonts noto-fonts-cjk noto-fonts-emoji                   `# Full unicode and font support`
 
 genfstab /mnt > /mnt/etc/fstab
 echo permit persist keepenv :wheel > /mnt/etc/doas.conf
