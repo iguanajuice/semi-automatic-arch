43717a34a8fea0ecab6904059a851792da5e0db3
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-07-20 18:43:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ LIBVA=mesa          # Driver for hardware video encoding/decoding: Radeon=mesa, 
 TZ=America/New_York # Your timezone (Region/City). Timezones can be found in /usr/share/zoneinfo
 
 sed -i 's/#Parallel/Parallel/g' /etc/pacman.conf # haha package download go brrrrr
-pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers $UCODE doas $_EDITOR $_SHELL `# Core packages` \
+pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers linux-firmware $UCODE doas $_EDITOR $_SHELL `# Core packages` \
 	grub efibootmgr                                                                 `# Bootloader packages` \
 	git wget htop neofetch man-db usbutils dmidecode arch-install-scripts           `# Miscellaneous CLI tools` \
 	btrfs-progs lvm2 ntfs-3g gvfs-mtp                                               `# Support for other filesystems` \
