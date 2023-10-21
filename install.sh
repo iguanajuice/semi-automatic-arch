9cdcaf6e9a84eadbbddc61b0236595947f55abca
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-10-21 06:25:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers linux-firmware
 	git wget neofetch man-db usbutils dmidecode                                     `# Miscellaneous CLI tools` \
 	btrfs-progs lvm2 ntfs-3g gvfs-mtp                                               `# Filesystem support` \
 	networkmanager net-tools wireless_tools iw                                      `# Networking` \
-	wireplumber pipewire-pulse pipewire-jack                                        `# Audio` \
+	wireplumber pipewire-pulse pipewire-jack pipewire-alsa                          `# Audio` \
 	libva-$LIBVA-driver gstreamer-vaapi                                             `# Hardware video codecs` \
  	gnu-free-fonts libertinus-font ttf-liberation ttf-ubuntu-font-family ttf-dejavu `# Extra fonts` \
   	noto-fonts noto-fonts-cjk noto-fonts-emoji                                      `# Full unicode support`
