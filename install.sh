a9501dcd61fd5adfd29647f5eb04b8d6c0223132
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-10-29 21:44:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sed -i 's/#Parallel/Parallel/g' /etc/pacman.conf # haha package download go brrr
 pacstrap -K /mnt --needed base base-devel $KERNEL $KERNEL-headers linux-firmware $UCODE $EDITOR $SHELL `# Core packages` \
 	grub efibootmgr                                                                 `# Bootloader` \
 	arch-install-scripts git wget neofetch man-db usbutils dmidecode                `# Miscellaneous CLI tools` \
-	btrfs-progs lvm2 ntfs-3g gvfs-mtp                                               `# Filesystem support` \
+	btrfs-progs lvm2 ntfs-3g gvfs-mtp                                               `# Extra filesystem support` \
 	networkmanager net-tools wireless_tools iw                                      `# Networking` \
 	wireplumber pipewire-pulse pipewire-jack pipewire-alsa                          `# Audio` \
 	libva-$LIBVA-driver gstreamer-vaapi                                             `# Hardware video codecs` \
